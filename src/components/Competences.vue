c10b0a5e95c77c3ad6a5b7297a9909aba1bb205a
Mathias Top <mtop@localhost.localdomain>
2020-03-25 19:45:46 +0100

    [~] smAndDown competences

--- src/components/Competences.vue
+++ src/components/Competences.vue
@@ -24,6 +24,31 @@
           </v-list>
         </v-col>
       </v-row>
+
+      <v-row v-show="$vuetify.breakpoint.smAndDown">
+        <v-col cols="1"></v-col>
+        <v-col cols="10">
+          <div style="font-size: 130px; font-weight: bold; color: darkblue">
+            Compétences
+          </div>
+          <v-list>
+            <v-list-item>
+              <v-icon size="100" class="change-color">mdi-docker</v-icon>
+              <v-icon size="100" class="change-color">mdi-kubernetes</v-icon>
+              <v-divider vertical/>
+              <v-icon size="100" class="change-color">mdi-language-php</v-icon>
+              <v-icon size="100" class="change-color">mdi-symfony</v-icon>
+              <v-divider vertical/>
+              <v-icon size="100" class="change-color">mdi-vuejs</v-icon>
+              <v-icon size="100" class="change-color">mdi-vuetify</v-icon>
+              <v-divider vertical/>
+              <v-icon size="100" class="change-color">mdi-language-c</v-icon>
+              <v-icon size="100" class="change-color">mdi-language-cpp</v-icon>
+              <v-icon size="100" class="change-color">mdi-language-python</v-icon>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
     </v-container>
 </template>
 
